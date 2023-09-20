4d2fcf704ba8d94a8c5e32cda64623ef8df4081e
SiddheshKasat26 <142038176+SiddheshKasat26@users.noreply.github.com>
2023-09-20 18:40:23 +0530

    Update main.py

--- Day_15 - Coffee_Machine/main.py
+++ Day_15 - Coffee_Machine/main.py
@@ -31,6 +31,43 @@ resources = {
     "coffee": 100,
 }
 
+
+def is_resources_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient."""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("How many Quarters?: ")) * 0.25
+    total += int(input("How many Dimes?: ")) * 0.10
+    total += int(input("How many Nickels?: ")) * 0.05
+    total += int(input("How many Pennies?: ")) * 0.01
+    return total
+
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+
+
 should_run = True
 
 while should_run:
@@ -45,7 +82,7 @@ while should_run:
         print(f"Coffee: {resources['coffee']}g")
         print(f"Money: ${profit}")
     else:
-        
-
-
-
+        drink = MENU[choice]
+        if is_resources_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
